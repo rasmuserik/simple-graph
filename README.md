c91177fac65aa3ee1ef43a785ceb06af9d4cb42d
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-04-06 17:00:45 +0200

    initialised via javascript-template.solsort.com/

---
+++ README.md
@@ -0,0 +1,1 @@
+**See <http://simple-graph.solsort.com/>**
---
+++ simple-graph.js
@@ -0,0 +1,6 @@
+// # JavaScript simple-graph
+//
+// Template for new solsort projects.
+//
+// Includes minimal webpack config, test-runner, etc.
+//
